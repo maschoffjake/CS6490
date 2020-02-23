1e4f575096e4d6c864f9b2fd15e5eb5b466e09e0
Jake Maschoff <maschoffjake@gmail.com>
2020-02-23 14:05:49 -0700

    bob counters enabled.

--- PA1/Expanded_Needham_Schroeder/alice.py
+++ PA1/Expanded_Needham_Schroeder/alice.py
@@ -12,11 +12,10 @@ MESSAGE_SIZE = 1024
 
 def client(name):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('127.0.0.1', 5000))
+    s.connect(('127.0.0.1', 5001))
     s.send(name)
     data = s.recv(1024)
-    result = json.loads(data)
-    print(json.dumps(result, indent=4))
+    print(data)
 
 def main():
 
--- PA1/Expanded_Needham_Schroeder/bob.py
+++ PA1/Expanded_Needham_Schroeder/bob.py
@@ -5,24 +5,36 @@ import argparse
 import socket
 import sys
 import threading
-import json
-from Crypto.Cipher import DES
+import os
+from Crypto.Cipher import DES3
+from Crypto import Random
 
 HOST = 'localhost'
 PORT = 5001
 
-kb = b'4DB7CAA9A2B14'
+kb = b'DC8FA9EE9BA7FTTA'
+iv = b'\x81\xde\xa6\xf3u\x9d\x11\xdd'
 
 MESSAGE_SIZE = 1024
 
-def handle_auth():
-    
-    # Send back a random challenge...
+def handle_auth(conn, address, cbc):
+    # Send back a random challenge... 64-bit challenges!
+    rnd = Random.new()
+    nb = rnd.read(8)
+    # nb = b'\x00\xa5\xf6\xfe4\xc5\r\xe5'
+    if cbc:
+        # CBC requires an IV (intialization vector)
+        cipher = DES3.new(kb, DES3.MODE_CBC, iv)
+    else:
+        cipher = DES3.new(kb, DES3.MODE_ECB)
+
+    msg = cipher.encrypt(nb)
+    conn.sendall(msg)
 
 # Function used to start the KDC server
 # Once a connection is made, it creates a new thread
 # and passes it off to a new function (handle_kdc)
-def start_server():
+def start_server(cbc=False):
     print('Bob is listening...')
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -30,7 +42,7 @@ def start_server():
     s.listen(5)
     while True:
         (conn, address) = s.accept()
-        t = threading.Thread(target=handle_auth)
+        t = threading.Thread(target=handle_auth, args=(conn, address, cbc))
         t.daemon = False
         t.start()
 
@@ -41,10 +53,14 @@ def main():
     args = parser.parse_args()
     if args.ecb:
         print('ECB!')
+        try:
+            start_server()
+        except Exception as e:
+            print('Received exception:', e)
     else:
         print('CBC!')
         try:
-            start_server()
+            start_server(cbc=True)
         except Exception as e:
             print('Received exception:', e)
 
